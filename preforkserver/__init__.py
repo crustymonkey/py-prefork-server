85662b98266c8d3d6e6d1a3dd069e31973721f1a
Jay Deiman <admin@splitstreams.com>
2012-09-10 12:25:24 -0700

    version bump

--- preforkserver/__init__.py
+++ preforkserver/__init__.py
@@ -18,7 +18,7 @@
 #    along with py-prefork-server.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from BaseChild import *
 from Manager import *
