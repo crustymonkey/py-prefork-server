fe6c40c3912d7670a2b5e447c3b67e07415a675e
Cosmin Basca <cosmin.basca@gmail.com>
2014-05-21 23:58:34 +0200

    PEP 8 api compliance
    Made many API renaming from CamelCase to the pythonic camel_case.
    Renamed modules according to PEP 8. Introduced a new address property
    in Manager that returns the server socket bound address. Renamed
    various internal variables to match PEP 8. Renames accSocket to
    server_socket.  Optimised imports (faster code execution by avoiding
    unnecessary lookups). Moved exceptions to a new exceptions module.
    
    This is a first attempt to make the api compliant with PEP 8.
    Preemptively bumped the version to 0.2.1 but this is not necessary

--- preforkserver/Manager.py
+++ preforkserver/Manager.py
@@ -18,106 +18,119 @@
 #    along with py-prefork-server.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import ChildEvents as ce
-import multiprocessing as mp
-import threading , socket , signal , select , os
+from preforkserver.exceptions import ManagerError
+from preforkserver.events import WAITING, CLOSE, EXITING, EXITING_ERROR, BUSY
+from multiprocessing import Pipe
+from threading import Event, Thread
+from signal import SIGHUP, SIGINT, SIGTERM, signal
+from select import poll, POLLIN, POLLPRI
+import select
+from socket import SOCK_STREAM, SOCK_DGRAM, socket, AF_INET, SOL_SOCKET, SO_REUSEADDR
+import os
 
 __all__ = ['Manager']
 
-class ManagerError(Exception):
-    pass
 
 class ManagerChild(object):
     """
     Class to represent a child in the Manager
     """
-    def __init__(self , pid , parConn):
+
+    def __init__(self, pid, parent_conn):
         self.pid = pid
-        self.conn = parConn
-        self.curState = ce.WAITING
-        self.totalProcessed = 0
+        self.conn = parent_conn
+        self.current_state = WAITING
+        self.total_processed = 0
 
     def close(self):
         self.conn.close()
+
 
 class Manager(object):
     """
     This class manages all the child processes.
     """
-    validProtocols = ('udp' , 'tcp')
+    validProtocols = ('udp', 'tcp')
 
-    def __init__(self , childClass , maxServers=20 , minServers=5 , 
-            minSpareServers=2 , maxSpareServers=10 , maxRequests=0 ,  
-            bindIp='127.0.0.1' , port=10000 , proto='tcp' , listen=5):
+    def __init__(self, child_class, max_servers=20, min_servers=5,
+                 min_spare_servers=2, max_spare_servers=10, max_requests=0,
+                 bind_ip='127.0.0.1', port=10000, protocol='tcp', listen=5):
         """
-        childClass<BaseChild>       : An implentation of BaseChild to define
+        child_class<BaseChild>       : An implentation of BaseChild to define
                                       the child processes
-        maxServers<int>             : Maximum number of children to have
-        minServers<int>             : Minimum number of children to have
-        minSpareServers<int>        : Minimum number of spare children to have
-        maxSpareServers<int>        : Maximum number of spare children to have
-        maxRequests<int>            : Maximum number of requests each child
+        max_servers<int>             : Maximum number of children to have
+        min_servers<int>             : Minimum number of children to have
+        min_spare_servers<int>       : Minimum number of spare children to have
+        max_spare_servers<int>       : Maximum number of spare children to have
+        max_requests<int>            : Maximum number of requests each child
                                       should handle.  Zero is unlimited and
                                       default
-        bindIp<str>                 : The IP address to bind to
-        port<int>                   : The port that the server should listen on
-        proto<str>                  : The protocol to use (tcp or udp)
-        listen<int>                 : Listen backlog
+        bind_ip<str>                 : The IP address to bind to
+        port<int>                    : The port that the server should listen on
+        protocol<str>                  : The protocol to use (tcp or udp)
+        listen<int>                  : Listen backlog
         """
-        self.childClass = childClass
-        self.maxServers = int(maxServers)
-        self.minServers = int(minServers)
-        if self.minServers > self.maxServers:
+        self._ChildClass = child_class
+        self.max_servers = int(max_servers)
+        self.min_servers = int(min_servers)
+        if self.min_servers > self.max_servers:
             raise ManagerError('You cannot have minServers '
-                '(%d) be larger than maxServers (%d)!' % 
-                (minServers , maxServers))
-        self.minSpares = int(minSpareServers)
-        self.maxSpares = int(maxSpareServers)
-        if self.minSpares > self.maxSpares:
+                               '(%d) be larger than maxServers (%d)!' %
+                               (min_servers, max_servers))
+        self.min_spares = int(min_spare_servers)
+        self.max_spares = int(max_spare_servers)
+        if self.min_spares > self.max_spares:
             raise ManagerError('You cannot have minSpareServers be larger '
-                'than maxSpareServers!')
-        self.maxReqs = int(maxRequests)
-        self.bindIp = bindIp
+                               'than maxSpareServers!')
+        self.max_requests = int(max_requests)
+        self.bind_ip = bind_ip
         self.port = int(port)
-        self.proto = proto.lower()
-        if proto not in self.validProtocols:
-            raise ManagerError('Invalid protocol %s, must be in: %r' % (proto ,
-                self.validProtocols))
+        self.protocol = protocol.lower()
+        if protocol not in self.validProtocols:
+            raise ManagerError('Invalid protocol %s, must be in: %r' % (protocol,
+                                                                        self.validProtocols))
         self.listen = int(listen)
-        self.accSock = None
-        self._stop = threading.Event()
+        self.server_socket = None
+        self._stop = Event()
         self._children = {}
-        self._poll = select.poll()
-        self._pollMask = select.POLLIN | select.POLLPRI
+        self._poll = poll()
+        self._pollMask = POLLIN | POLLPRI
 
         # Bind the socket now so that it can be used before run is called
         # Addresses: https://github.com/crustymonkey/py-prefork-server/pull/3
-        self.preBind()
+        self.pre_bind()
         self._bind()
-        self.postBind()
+        self.post_bind()
 
-    def _startChild(self):
-        parPipe , chPipe = mp.Pipe()
-        self._poll.register(parPipe.fileno() , self._pollMask)
+    @property
+    def address(self):
+        """
+        returns the newly bound server address as an (ip, port) tuple
+        """
+        return self.server_socket.getsockname()
+
+    def _start_child(self):
+        parent_pipe, child_pipe = Pipe()
+        self._poll.register(parent_pipe.fileno(), self._pollMask)
         pid = os.fork()
         if not pid:
-            ch = self.childClass(self.accSock , self.maxReqs , chPipe , 
-                self.proto)
-            parPipe.close()
+            ch = self._ChildClass(self.server_socket, self.max_requests, child_pipe,
+                                  self.protocol)
+            parent_pipe.close()
             ch.run()
         else:
-            self._children[parPipe.fileno()] = ManagerChild(pid , parPipe)
-            chPipe.close()
+            self._children[parent_pipe.fileno()] = ManagerChild(pid, parent_pipe)
+            child_pipe.close()
             return
 
-    def _killChild(self , child , background=True):
+    def _kill_child(self, child, background=True):
         """
         Kill a ManagerChild, child, off.  If background is True, wait for 
         completion in a thread.
         """
         fd = child.conn.fileno()
         try:
-            child.conn.send([ce.CLOSE , ''])
+            child.conn.send([CLOSE, ''])
             child.close()
         except IOError:
             pass
@@ -128,84 +141,84 @@ class Manager(object):
         if fd in self._children:
             del self._children[fd]
         if background:
-            t = threading.Thread(target=os.waitpid , args=(child.pid , 0))
+            t = Thread(target=os.waitpid, args=(child.pid, 0))
             t.daemon = True
             t.start()
         else:
-            os.waitpid(child.pid , 0)
+            os.waitpid(child.pid, 0)
 
-    def _handleChildEvent(self , child):
-        event , msg = child.conn.recv()
+    def _handle_child_event(self, child):
+        event, msg = child.conn.recv()
         event = int(event)
-        if event & ce.EXITING:
-            if event == ce.EXITING_ERROR:
-                self.log('Child %d exited due to error: %s' % (child.pid , msg))
+        if event & EXITING:
+            if event == EXITING_ERROR:
+                self.log('Child %d exited due to error: %s' % (child.pid, msg))
             fd = child.conn.fileno()
             self._poll.unregister(fd)
             del self._children[fd]
             child.close()
-            os.waitpid(child.pid , 0)
+            os.waitpid(child.pid, 0)
         else:
-            child.curState = int(event)
-            child.totalProcessed = int(msg)
+            child.current_state = int(event)
+            child.total_processed = int(msg)
 
-    def _assessState(self):
+    def _assess_state(self):
         """
         Check the state of all the children and handle startups and shutdowns
         accordingly
         """
-        totalBusy = 0
+        total_busy = 0
         children = self._children.values()
-        numChildren = len(children)
+        num_children = len(children)
         for ch in children:
-            if ch.curState & ce.BUSY:
-                totalBusy += 1
-        spares = numChildren - totalBusy
-        if spares < self.minSpares:
+            if ch.curState & BUSY:
+                total_busy += 1
+        spares = num_children - total_busy
+        if spares < self.min_spares:
             # We need to fork more children
-            diff2max = self.maxServers - numChildren
-            toFork = spares
+            diff2max = self.max_servers - num_children
+            to_fork = spares
             if diff2max - spares < 0:
-                toFork = diff2max
-            for i in xrange(toFork):
-                self._startChild()
-        elif spares > self.maxSpares + self.minServers:
+                to_fork = diff2max
+            for i in xrange(to_fork):
+                self._start_child()
+        elif spares > self.max_spares + self.min_servers:
             # We have too many spares and need to kill some
-            toKill = spares - self.maxSpares
-            children = sorted(children ,
-                cmp=lambda x,y: cmp(x.totalProcessed , y.totalProcessed) ,
-                reverse=True)
+            to_kill = spares - self.max_spares
+            children = sorted(children,
+                              cmp=lambda x, y: cmp(x.totalProcessed, y.totalProcessed),
+                              reverse=True)
             # Send closes
-            for ch in children[:toKill]:
-                self._killChild(ch)
-        if numChildren < self.minServers:
-            for i in xrange(self.minServers - numChildren):
-                self._startChild()
+            for ch in children[:to_kill]:
+                self._kill_child(ch)
+        if num_children < self.min_servers:
+            for i in xrange(self.min_servers - num_children):
+                self._start_child()
 
-    def _initChildren(self):
-        for i in range(self.minServers):
-            self._startChild()
+    def _init_children(self):
+        for i in range(self.min_servers):
+            self._start_child()
 
     def _bind(self):
         """
         Bind the socket
         """
-        addr = (self.bindIp , self.port)
-        proto = socket.SOCK_STREAM
-        if self.proto == 'udp':
-            proto = socket.SOCK_DGRAM
-        self.accSock = socket.socket(socket.AF_INET , proto)
-        self.accSock.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
-        self.accSock.settimeout(0.01)
-        self.accSock.bind(addr)
-        if self.proto == 'tcp':
-            self.accSock.listen(self.listen)
+        address = (self.bind_ip, self.port)
+        protocol = SOCK_STREAM
+        if self.protocol == 'udp':
+            protocol = SOCK_DGRAM
+        self.server_socket = socket(AF_INET, protocol)
+        self.server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.server_socket.settimeout(0.01)
+        self.server_socket.bind(address)
+        if self.protocol == 'tcp':
+            self.server_socket.listen(self.listen)
 
-    def _signalSetup(self):
+    def _signal_setup(self):
         # Set the signal handlers
-        signal.signal(signal.SIGHUP , self.hupHandler)
-        signal.signal(signal.SIGINT , self.intHandler)
-        signal.signal(signal.SIGTERM , self.termHandler)
+        signal(SIGHUP, self.hup_handler)
+        signal(SIGINT, self.int_handler)
+        signal(SIGTERM, self.term_handler)
 
     def _loop(self):
         while True:
@@ -218,41 +231,41 @@ class Manager(object):
                 # When a signal is received, it can interrupt the system call
                 # and break things with an improper exit
                 pass
-            for fd , e in events:
+            for fd, e in events:
                 if fd in self._children:
                     ch = self._children[fd]
-                    self._handleChildEvent(ch)
+                    self._handle_child_event(ch)
                 else:
                     try:
                         self._poll.unregister(fd)
-                    except Exception , e:
-                        self.log('Error unregistering %d: %s; %s' % (fd , e))
+                    except Exception, e:
+                        self.log('Error unregistering %d: %s; %s' % (fd, e))
                     try:
                         os.close(fd)
-                    except Exception , e:
+                    except Exception, e:
                         self.log('Error closing child pipe: %s' % e)
-            self._assessState()
-            
-    def _shutdownServer(self):
+            self._assess_state()
+
+    def _shutdown_server(self):
         self.log('Starting server shutdown')
         children = self._children.values()
         # First loop through and tell the children to close
-        for ch in children:
-            self._killChild(ch , False)
-        self.accSock.close()
+        for child in children:
+            self._kill_child(child, False)
+        self.server_socket.close()
         self.log('Server shutdown completed')
-    
+
     def run(self):
-        self.preSignalSetup()
-        self._signalSetup()
-        self.postSignalSetup()
-        self.preInitChildren()
-        self._initChildren()
-        self.postInitChildren()
-        self.preLoop()
+        self.pre_signal_setup()
+        self._signal_setup()
+        self.post_signal_setup()
+        self.pre_init_children()
+        self._init_children()
+        self.post_init_children()
+        self.pre_loop()
         self._loop()
-        self.preServerClose()
-        self._shutdownServer()
+        self.pre_server_close()
+        self._shutdown_server()
 
     def close(self):
         """
@@ -261,7 +274,7 @@ class Manager(object):
         self._stop.set()
 
     # All of the following methods can be overridden in a subclass
-    def preBind(self):
+    def pre_bind(self):
         """
         This hook is called before the main socket is created and bound
         to the ip:port.  This is similar to the initialize() hook in the
@@ -269,46 +282,46 @@ class Manager(object):
         """
         return
 
-    def postBind(self):
+    def post_bind(self):
         """
         As you might have guessed, this is called right after the accept()
         socket has been created and bound.
         """
         return
 
-    def preSignalSetup(self):
+    def pre_signal_setup(self):
         """
         This is called before the signal handlers are set up
         """
         return
 
-    def postSignalSetup(self):
+    def post_signal_setup(self):
         """
         This is called after the signal handlers have been set.  You can
         override the default signal handlers if you like.  More on that below.
         """
         return
 
-    def preInitChildren(self):
+    def pre_init_children(self):
         """
         This is called before the child processes are initialized
         """
         return
 
-    def postInitChildren(self):
+    def post_init_children(self):
         """
         This is called after the child processes are initialized
         """
         return
 
-    def preLoop(self):
+    def pre_loop(self):
         """
         This is the last hook before the main server loop takes over.  
         Any last minute setup items you wish to do should be done here
         """
         return
 
-    def preServerClose(self):
+    def pre_server_close(self):
         """
         This is called before the server shuts down.  Any cleanup you wish
         to take care of before termination should be done here.
@@ -316,26 +329,26 @@ class Manager(object):
         return
 
     # Signal handling.  These can be overridden in a subclass as well
-    def hupHandler(self , frame , num):
+    def hup_handler(self, frame, num):
         """
         Handle a SIGHUP.  By default, this does nothing
         """
         return
 
-    def intHandler(self , frame , num):
+    def int_handler(self, frame, num):
         """
         Handle a SIGINT.  By default, this will stop the server
         """
         self._stop.set()
 
-    def termHandler(self , frame , num):
+    def term_handler(self, frame, num):
         """
         Handle a SIGTERM.  By default, this will stop the server
         """
         self._stop.set()
 
     # Utilities that can be defined
-    def log(self , msg):
+    def log(self, msg):
         """
         You can define a logging method and log internal messages and messages
         you generate.  By default, this does nothing.
--- preforkserver/exceptions.py
+++ preforkserver/exceptions.py
@@ -1,7 +1,7 @@
 #
 #    Author: Jay Deiman
 #    Email: admin@splitstreams.com
-# 
+#
 #    This file is part of py-prefork-server.
 #
 #    py-prefork-server is free software: you can redistribute it and/or modify
@@ -18,28 +18,5 @@
 #    along with py-prefork-server.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-"""
-This is just a file defining the child state events
-"""
-# Sent from child: Child is waiting for a connection
-WAITING = 1
-# Sent from child: Child is busy handling a connection
-BUSY = 2
-# Sent from child: Child is exiting due to an error
-EXITING_ERROR = 4
-# Sent from child: Child is exiting due to processing max requests
-EXITING_MAX = 8
-EXITING = EXITING_ERROR | EXITING_MAX
-# Sent from manager (parent): Tell the child to exit after handling it's
-# current request
-CLOSE = 16
-
-# A dictionary to map the event numbers to strings
-STRMAP = {
-    WAITING: 'WAITING' ,
-    BUSY: 'BUSY' ,
-    EXITING_ERROR: 'EXITING_ERROR' ,
-    EXITING_MAX: 'EXITING_MAX' ,
-    EXITING: 'EXITING' ,
-    CLOSE: 'CLOSE' ,
-}
+class ManagerError(Exception):
+    pass
